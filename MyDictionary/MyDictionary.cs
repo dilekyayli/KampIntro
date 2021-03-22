92c13b01227a3a28898ec7f1faeb1ee871e693bb
Dilek YAYLI <dilek_364@hotmail.com>
2021-03-22 03:38:00 +0300

    mydictionary

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] Keys;
+        V[] Values;
+        public MyDictionary()
+        {
+            Keys = new K[0];
+            Values = new V[0];
+        }
+        public void Add(K key, V value)
+        {
+            K[] tempArrayK = Keys;
+            V[] tempArrayV = Values;
+            Keys = new K[Keys.Length + 1];
+            Values = new V[Values.Length + 1];
+
+            for (int i = 0; i <tempArrayK.Length; i++)
+            {
+                Keys[i] = tempArrayK[i];
+                Values[i] = tempArrayV[i];
+            }
+            Keys[Keys.Length - 1] = key;
+            Values[Values.Length - 1] = value;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("Dilek", "Yaylı");
+        }
+    }
+}
